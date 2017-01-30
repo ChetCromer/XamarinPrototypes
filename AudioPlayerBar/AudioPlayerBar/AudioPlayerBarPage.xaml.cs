cec6e699280acd5fc518b60bb25510b2de62eb3d
Chet Cromer <chetcromer@c2itconsulting.net>
2017-01-30 15:09:52 -0500

    Started AudioPlayerBar

---
+++ AudioPlayerBar/AudioPlayerBar/AudioPlayerBarPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace AudioPlayerBar
+{
+	public partial class AudioPlayerBarPage : ContentPage
+	{
+		public AudioPlayerBarPage()
+		{
+			InitializeComponent();
+		}
+
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			Navigation.PushAsync(new AudioPlayerBarPage());
+		}
+	}
+}
