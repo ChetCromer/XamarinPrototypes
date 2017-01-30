33f1b2f24a74e683d1e1574de773a28c1fa5512e
Chet Cromer <chetcromer@c2itconsulting.net>
2017-01-30 14:21:03 -0500

    Initial Commit of custom nav.

---
+++ CustomNavigationPage/CustomNavigationPage/MyNavigationPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CustomNavigationPage
+{
+	public partial class MyNavigationPage : NavigationPage	
+	{
+		public MyNavigationPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
